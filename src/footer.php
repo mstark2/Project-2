c75d744647135bc21e4d7533ac4a73f257fc0e73
Morgan Stark <mstark2@umbc.edu>
2015-11-16 11:39:39 -0500

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,5 +1,7 @@
 <?php
 
-<footer>&copy; University of Maryland, Baltimore County <span class="bullet">&bull;</span> 1000 Hilltop Circle <span class="bullet">&bull;</span> Baltimore, MD 21250 </footer>
+<footer>
+  &copy; University of Maryland, Baltimore County <!-- <span class="bullet">&bull;</span> 1000 Hilltop Circle <span class="bullet">&bull;</span> Baltimore, MD 21250 -->
+</footer>
 
 ?>
