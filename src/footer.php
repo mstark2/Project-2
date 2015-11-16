5be0dd7f4f399e7e1abfd0ded80c23ae7958a8c8
Sami Turskey <snturskey@umbc.edu>
2015-11-16 11:42:37 -0500

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -2,6 +2,6 @@
 
 echo("<footer>
   &copy; University of Maryland, Baltimore County <!-- <span class="bullet">&bull;</span> 1000 Hilltop Circle <span class="bullet">&bull;</span> Baltimore, MD 21250 -->
-</footer>")
+</footer>");
 
 ?>
