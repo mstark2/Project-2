13a94f9fe8072345ad3aa3a0ad8ca38bf53ea655
Sami Turskey <snturskey@umbc.edu>
2015-11-30 16:35:09 -0500

    update max dates on when apps can be schecduled

--- src/AdminScheduleInd.php
+++ src/AdminScheduleInd.php
@@ -33,7 +33,7 @@ session_start();
 		<!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 		<!-- %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
 
-	      <input id="Date" type="date" name="Date" placeholder="mm/dd/yyyy" min="2015-08-01" max="2015-10-30" required autofocus> (mm/dd/yyyy)
+	      <input id="Date" type="date" name="Date" placeholder="mm/dd/yyyy" min="2015-08-01" max="2016-10-30" required autofocus> (mm/dd/yyyy)
 	    </div>
             <!-- Possible appointment times to choose from -->
 	    <div class="field">
