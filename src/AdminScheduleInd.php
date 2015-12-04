364ebddedaf4ac807c9d66cd2979d58cb465bc20
Morgan Stark <mstark2@umbc.edu>
2015-12-04 11:13:39 -0500

    Update AdminScheduleInd.php

--- src/AdminScheduleInd.php
+++ src/AdminScheduleInd.php
@@ -64,7 +64,7 @@ session_start();
           <input type="checkbox" name="major[]" value="CMSC" checked>Computer Science
           <input type="checkbox" name="major[]" value="MENG" checked>Mechanical Engineering
           <input type="checkbox" name="major[]" value="CENG" checked>Chemical Engineering
-          <input type="checkbox" name="major[]" value="Engineering" checked>Engineering
+          <input type="checkbox" name="major[]" value="ENGR" checked>Engineering
       </div>
       
       <div class="field">
