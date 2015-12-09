989299e900b26675a2c70f8dc4e7ce156ec29bc2
conradte3 <conradte3@gmail.com>
2015-12-09 15:35:05 -0500

    woops

--- src/src/workOrder/workButton.php
+++
@@ -1,3 +0,0 @@
-<br>
-	<img src="./images/workOrder_icon.gif" border="0" height="42" width="42" style="float: right;" onClick='window.open("./workOrder/workOrder.php?url=<?php echo($_SERVER['SCRIPT_NAME']); ?>", "", "width=800, height=600" )' title="Issue work order"/>
-<br>
